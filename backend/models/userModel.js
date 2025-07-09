0ef1495cb875012f5dcac02e3f6fa7cfb327e162
Jagdishuikey <161849549+Jagdishuikey@users.noreply.github.com>
2025-07-09 20:20:50 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,11 +12,6 @@ const userSchema = new Schema({
         required: true,
         unique: true
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
     photo:{
         type: String,
         required: true
